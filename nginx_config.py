8c3049d47e4f7b2f4c7c4469df66577590643645
Brian Hicks <brian@brianthicks.com>
2024-04-16 06:29:26 -0500

    add a simple nginx config

---
+++ nginx_config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+with open('sources.txt', 'r') as fh:
+    sources = [line.split() for line in fh.read().strip().split('\n')]
+
+# preamble
+print("""\
+server {
+    listen 80;
+    server_name git.bytes.zone;
+
+    location / {
+        return 301 $scheme://github.com$request_uri;
+    }
+""")
+
+for (src, dest) in sources:
+    print(f"    location ~* ^/{src}/commit/([0-9a-f]+)$ {{")
+    print(f"        return 301 https://github.com/{dest}/commit/$1;")
+    print("    }")
+
+print("}")
