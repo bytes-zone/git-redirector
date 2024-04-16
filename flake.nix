03d4a85198574d630a8fad22b9df3af957fdede9
Brian Hicks <brian@brianthicks.com>
2024-04-16 06:46:02 -0500

    produce a config derivation

--- flake.nix
+++ flake.nix
@@ -7,10 +7,23 @@
   outputs = inputs:
     inputs.flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import inputs.nixpkgs { inherit system; };
-      in { 
+      in {
         formatter = pkgs.nixpkgs-fmt;
 
         devShell = pkgs.mkShell { packages = [ ]; };
+
+        packages.config = pkgs.stdenv.mkDerivation {
+          name = "generator";
+          src = ./src;
+
+          nativeBuildInputs = [ pkgs.python3 ];
+
+          installPhase = ''
+            mkdir -p $out/etc/nginx
+
+            python3 nginx_config.py sources.txt > $out/etc/nginx/nginx.conf
+          '';
+        };
       }
     );
 }
